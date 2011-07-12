d7152292821c7b701fb02b0840bf580cb5a5aed7
s.deivasigamani@gmail.com <s.deivasigamani@gmail.com@b6d1bee5-c932-4a0f-eef7-4feec481f256>
2011-07-12 14:11:28 +0000

    Comments implemented

--- src/in/espirit/tracer/database/dao/TicketDao.java
+++ src/in/espirit/tracer/database/dao/TicketDao.java
@@ -9,7 +9,6 @@ import in.espirit.tracer.model.Ticket;
 
 import java.sql.Connection;
 import java.sql.ResultSet;
-import java.sql.SQLException;
 import java.sql.Statement;
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
